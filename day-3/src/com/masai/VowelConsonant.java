817255b68eb7e5fb8964165a10b631fdce5480cc
moh shahrukh khan <shahrukhturk9@gmail.com>
2022-05-18 21:50:58 +0530

    .

--- day-3/src/com/masai/VowelConsonant.java
+++ day-3/src/com/masai/VowelConsonant.java
@@ -1,11 +1,28 @@
 package com.masai;
 
+import java.util.*;
+
 public class VowelConsonant {
 
 	
 	void fun(char Char) {
 		if(Character.isAlphabetic(Char)) {
-			System.out.println(Char);
+			switch (Char) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println(Char + " is vowel");
+                break;
+            default:
+                System.out.println(Char + " is consonant");
+        }
 		}else {
 			System.out.println("error");
 		}
@@ -13,11 +30,15 @@ public class VowelConsonant {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		VowelConsonant s=new VowelConsonant();
-		char a='a';
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Alphabet1");
+		char a=sc.next().charAt(0);
 		s.fun( a);
-		char A ='A';
+		System.out.println("Enter the Alphabet2");
+		char A =sc.next().charAt(0);;
 		s.fun( A);
-		char num=10;
+		System.out.println("Enter the Alphabet3");
+		char num=sc.next().charAt(0);;
 		s.fun( num);
 	}
 
