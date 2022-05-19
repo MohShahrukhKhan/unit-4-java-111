c88a258d5acf920a3fa56bb0d32d4f32fdb06d9f
moh shahrukh khan <shahrukhturk9@gmail.com>
2022-05-19 22:04:48 +0530

    add day-2

--- Day-2/problem2/src/com/masai/Prime.java
+++ Day-2/problem2/src/com/masai/Prime.java
@@ -11,8 +11,10 @@ public class Prime {
 	private static void primFec(int number) {
 		// TODO Auto-generated method stub
 		 for(int i=1;i<=number;i++) {
-    		 if(number%i==0) System.out.print(i+" ");
-    		 else System.out.println("invalid Number");
+    		while(number%i==0) {
+    			System.out.print(i+" ");
+    			number/=i;
+
     	 }
 	}
 }
