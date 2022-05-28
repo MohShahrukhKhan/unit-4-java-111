b5369ce3868b67d663fad618a963d3cbb356b222
moh shahrukh khan <shahrukhturk9@gmail.com>
2022-05-29 00:04:03 +0530

    added Day-9 Assignment

---
+++ Day-9/Day-9-Parent/src/com/masai/child.java
@@ -0,0 +1,35 @@
+package com.masai;
+
+import java.util.Scanner;
+
+public final class child extends Parent{	
+	@Override
+	public void method1() {
+		System.out.println(getNumber());
+		System.out.println("I am inside method 1 of child");
+	}
+	
+	public void method4() {
+		System.out.println("I am inside method 4 of child");
+	}
+	
+	public child(int i) {
+		super(i);
+	}
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number between 1 to 10 : ");
+		int userInput = input.nextInt();
+		if(userInput >= 1 && userInput <= 10) {
+			Parent var = new child(userInput);
+			var.method1();
+			var.method2();
+			var.method3();
+		}
+		else {
+			System.out.println("Invalid number");
+		}
+		
+	}
+}
---
+++ Day-9/Day-9-Student/src/com/masai/Dome.java
@@ -0,0 +1,15 @@
+package com.masai;
+
+public class Dome extends Student{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student obj=new Student();
+		Student obj1=new Student();
+		obj.displayDetails();
+		System.out.println("**************************");
+		obj1.displayDetails();
+		
+	}
+
+}
